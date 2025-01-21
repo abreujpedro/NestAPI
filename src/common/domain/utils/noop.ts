f9df0910705135adf6b38acefe485975d592155d
João Pedro <pedro.abreu@loldesign.com.br>
2025-01-21 05:59:47 -0300

    fix: change create car params

--- src/common/domain/utils/noop.ts
+++ src/common/domain/utils/noop.ts
@@ -1,4 +1,4 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-export const noop = async (..._args: unknown[]): Promise<any> => {
+export const noop = async (..._args: unknown[]): Promise<void> => {
   /* do nothing */
 };
